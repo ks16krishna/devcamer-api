2368b1bd65c1af1120db8d0a2f0afe89d6bc5b7a
Krishna Sharma <ks16krishna@gmail.com>
2020-11-23 18:49:27 +0530

    async/await middleware

--- middleware/error.js
+++ middleware/error.js
@@ -4,11 +4,23 @@ const errorHandler = (err, req, res, next) => {
   let error = { ...err };
   error.message = err.message;
   //  Log to console for dev
-  console.log(err.stack.red);
+  console.log(err);
   // Mongoose bad ObjectID
   if (err.name === "CastError") {
     const message = `Bootcamp not found with id of ${err.value}`;
     error = new ErrorResponse(message, 404);
+  }
+
+  // Mongoose duplicate key
+  if (err.code === 11000) {
+    const message = "Duplicate field value entered";
+    error = new ErrorResponse(message, 400);
+  }
+
+  // Mongoose validation error
+  if (error.name === "ValidationError") {
+    const message = Object.values(err.errors).map((value) => val.meesage);
+    error = new ErrorResponse(message, 400);
   }
 
   res.status(error.statusCode || 500).json({
