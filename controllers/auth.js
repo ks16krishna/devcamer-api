a3cbbc54f17db7d415b8a2fe1a171f4f50b15af4
ks16krishna <ks16krishna@gmail.com>
2020-12-07 13:58:44 +0530

    xss protection & security headers

--- controllers/auth.js
+++ controllers/auth.js
@@ -82,6 +82,21 @@ exports.logout = asyncHandler(async (req, res, next) => {
   });
 });
 
+// @desc      Log user out / clear cookie
+// @route     GET /api/v1/auth/logout
+// @access    Private
+exports.logout = asyncHandler(async (req, res, next) => {
+  res.cooke("token", "none", {
+    expires: new Date(Date.now() + 10 * 1000),
+    httpOnly: true,
+  });
+
+  res.status(200).json({
+    success: true,
+    data: {},
+  });
+});
+
 // @desc      Get current logged in user
 // @route     GET /api/v1/auth/me
 // @access    Private
