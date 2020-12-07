c51c20b70f285a33899fe489c99c10c369b50b14
ks16krishna <ks16krishna@gmail.com>
2020-12-07 14:09:21 +0530

    bug fixes

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const cookieParser = require("cookie-parser");
 const mongoSanitize = require("express-mongo-sanitize");
 const helmet = require("helmet");
 const xss = require("xss-clean");
-const ratelimit = require("express-ratelimit");
+const rateLimit = require("express-rate-limit");
 const hpp = require("hpp");
 const cors = require("cors");
 const errorHandler = require("./middleware/error");
@@ -29,11 +29,11 @@ const reviews = require("./routes/reviews");
 
 const app = express();
 
-// Body Parser
+// Body parser
 app.use(express.json());
 
 // Cookie parser
-app.use(cookieParser);
+app.use(cookieParser());
 
 // Dev logging middleware
 if (process.env.NODE_ENV === "development") {
@@ -54,12 +54,12 @@ app.use(xss());
 
 // Rate limiting
 const limiter = rateLimit({
-  windowsMs: 10 * 60 * 1000, // 10 mins
+  windowMs: 10 * 60 * 1000, // 10 mins
   max: 100,
 });
 app.use(limiter);
 
-//Prevent http param pollution
+// Prevent http param pollution
 app.use(hpp());
 
 // Enable CORS
@@ -89,7 +89,6 @@ const server = app.listen(
 // Handle unhandled promise rejections
 process.on("unhandledRejection", (err, promise) => {
   console.log(`Error: ${err.message}`.red);
-
   // Close server & exit process
-  server.close(() => process.exit(1));
+  // server.close(() => process.exit(1));
 });
